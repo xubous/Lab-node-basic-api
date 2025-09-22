e6cc14ffbc8a8a80a9f2c76e7388d8f23a9abc74
Gabriel <gcs232005@gmail.com>
2025-09-22 15:21:16 -0300

    .

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require ('express')
+const cors = require('cors');
+const path = require ('path')
+const app = express ()
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/app', express.static (path.join (__dirname, '/public')))
+
+let port = process.env.PORT || 3000
+app.listen (port)
